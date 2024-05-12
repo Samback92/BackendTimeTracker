8673dddb215ed20a989db219bc804b13d607cc7f
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-05-12 10:27:51 +0200

    first commit

---
+++ src/main/java/com/backendtimetracker/backendtimetracker/BackendtimetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.backendtimetracker.backendtimetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtimetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtimetrackerApplication.class, args);
+	}
+
+}
